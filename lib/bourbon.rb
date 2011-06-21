69937e35c82bd562fcb4a64456cf97f5cb8429b3
Bob Nadler <bnadlerjr@gmail.com>
2011-06-21 19:59:57 -0400

    Label helper.

--- lib/bourbon.rb
+++ lib/bourbon.rb
@@ -1,6 +1,13 @@
 # == Description
 module Bourbon
-  def label(obj, field, display='', options={})
-    
+  # Generates a label tag. The 'for' attribute of the tag is scoped to the
+  # given +obj+ and +field+.
+  #
+  # Options::
+  #   :text::
+  #     Custom label text. Defaults to the field name if not given.
+  def label(obj, field, options={})
+    label_text = options.delete(:text) || field
+    "<label for=\"#{obj}[#{field}]\">#{label_text}</label>"
   end
 end
--- test/test_label_helper.rb
+++ test/test_label_helper.rb
@@ -3,9 +3,16 @@ require 'test_helper'
 class TestLabelHelper < Test::Unit::TestCase
   include Bourbon
 
-  def test_plain_label
+  def test_label_text_uses_field_if_not_given
     expected = '<label for="obj[item]">item</label>'
     actual = label :obj, :item
+
+    assert_equal expected, actual
+  end
+
+  def test_label_with_custom_text
+    expected = '<label for="obj[item]">My Label</label>'
+    actual = label :obj, :item, :text => 'My Label'
 
     assert_equal expected, actual
   end
